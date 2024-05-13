0603d25a0e510e307f3d4038e7e85f072e2fc406
Hans Pagel <mail@hanspagel.com>
2024-05-13 13:51:33 +0200

    chore: remove jsx renderer

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
 import galaxy from '@scalar/galaxy/latest.yaml?raw'
-// @ts-ignore
 import { createMockServer } from '@scalar/mock-server'
-
-import { renderer } from './renderer'
 
 // Create the mocked routes
 const app = await createMockServer({
   specification: galaxy,
 })
-
-app.use(renderer)
 
 export default app
